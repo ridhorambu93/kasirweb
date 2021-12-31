abc45d099cfd928d90400bdb480a585c12c0f7d7
ridhorambu93 <ridhodavybones@gmail.com>
2022-01-01 06:38:35 +0700

    menambahkan Auth Controller, membuat halaman login dan register

---
+++ application/views/admin/auth/register.php
@@ -0,0 +1,37 @@
+<!-- <div class="main"> -->
+    <section class="signup-content">
+        <div class="container">
+                <div class="row d-flex justify-content-center">
+                    <div class="col-md-12">
+                        <div class="card">
+                            <div class="card-header">
+                                <form action="#" method="POST">
+                                <h3 class="text-secondary text-center font-weight-bold text-capitalize mb-3">Form Registrasi</h3>
+                                    <div class="background-register">
+                                        <div class="form-group">
+                                            <input type="text" class="form-control" name="name" id="name" placeholder="Isi Nama anda" />
+                                        </div>
+                                        <div class="form-group">
+                                            <input type="email" class="form-control" name="email" id="email" placeholder="Isi Email anda" />
+                                        </div>
+                                        <div class="form-group">
+                                            <input type="text" class="form-control" name="password" id="password" placeholder="Isi Password" />
+                                            <span toggle="#password" class="zmdi zmdi-eye field-icon toggle-password"></span>
+                                        </div>
+                                        <div class="form-group">
+                                            <input type="password" class="form-control" name="re_password" id="re_password" placeholder="Ulangi Isi password anda" />
+                                        </div>
+                                        <div class="form-group">
+                                            <button class="btn btn-sm btn-primary" type="submit">Daftarkan Akun</button>
+                                        </div>
+                                    </div>
+                                </form>
+                                <p class="text-secondary mt-2">Sudah punya akun ? <a href="<?php echo base_url('admin/auth') ?>" class="hide-link">masuk disini</a></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+<!-- </div> -->
